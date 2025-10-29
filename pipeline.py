8aa3616ad6244075194be345ed5b5dc87b07b79c
Anton Korotkov <anton.s.korotkov@gmail.com>
2025-10-29 01:53:03 +0100

    Add pipeline draft (combining all parts together)

---
+++ pipeline.py
@@ -0,0 +1,51 @@
+import sys
+
+from .parser.parser import *
+from .typeinference.type_annotator import *
+from .interpreter.interpreter import *
+from .interpreter.builtins_ import *
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python pipeline.py <filename>")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            code = f.read()
+
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+        sys.exit(1)
+
+    # Tokenize
+    tokens = tokenize(code)
+
+    # Parse
+    parser = Parser(tokens)
+    ast = parser.parse()
+
+    # for debug
+    with open('output.txt', 'w', encoding='utf-8') as f:
+        pprint(ast, stream=f)
+
+    # Infer Types
+    # tast = type_annotate_program(ast)
+
+    # Interpret TAST
+    interp = Interpreter(ast)
+    global_frame = interp.run()
+
+    # Call main()
+    call = FunctionCall(function=VarRef("main"), arguments=[])
+    interp.eval_expression(call, global_frame)
+
+    sys.exit(0)
+    # except Exception as e:
+    #     print(f"{type(e).__name__}: {e}")
+    #     sys.exit(1)
+
+if __name__ == '__main__':
+    main()
